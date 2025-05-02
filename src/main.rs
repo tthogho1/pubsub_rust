c873e50a6d602b75111b4130f5c3fecfd22f8055
t-tougou <t-tougou@ndics.co.jp>
2025-05-02 22:57:10 +0900

    create receive task for testing multi receive task

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::time::Duration;
 use bb8_redis::RedisConnectionManager;
 use tokio;
 
+use tokio::sync::broadcast;
+
+
 pub async fn create_pool(redis_url: &str) -> Result<bb8::Pool<RedisConnectionManager>, redis::RedisError> {
     let manager = RedisConnectionManager::new(redis_url)?;
     let pool = bb8::Pool::builder()
@@ -52,30 +55,77 @@ fn connect_with_retry(client: &redis::Client, max_retries: u32) -> redis::RedisR
 async fn main() -> redis::RedisResult<()> {
     dotenv::dotenv().ok();
 
+    let (tx, mut rx1) = broadcast::channel(16);
+    let mut rx2: broadcast::Receiver<String> = tx.subscribe(); // 新しい受信機を作成
+
     let redis_url = env::var("REDIS_URL") .unwrap_or("redis://127.0.0.1:6379/".to_string());
     let redis_url_clone = redis_url.clone();
  
     let client = Client::open(redis_url)?;
     let mut con = connect_with_retry(&client, 10)?;
-    let mut pubsub = con.as_pubsub();
-    pubsub.subscribe("my_channel")?;
+    con.as_pubsub().subscribe("my_channel")?;
 
-    let _handle = tokio::spawn(async move {
-        if let Err(e) = publish_message(redis_url_clone).await{
+    let mut pubsub_task = con; // Make pubsub_task mutable
+
+    //
+    // spawn task to subscribe message
+    //
+    let _handle1 = tokio::spawn(async move {
+        let mut pubsub = pubsub_task.as_pubsub(); // Use pubsub_task as_pubsub directly
+        loop {
+            let msg = match pubsub.get_message() {
+                Ok(message) => {
+                    let payload = message.get_payload().unwrap_or_else(|_| {
+                        println!("Failed to get payload");
+                        String::new()
+                    });
+                    // send to broadcast channel
+                    if let Err(e) = tx.send(payload.clone()) {
+                        println!("Failed to send message to broadcast channel: {}", e);
+                    }
+
+                    Ok((message, payload))
+                }
+                Err(e) => {
+                    println!("Failed to get message: {}", e);
+                    Err(e)
+                }
+            };
+
+            match msg {
+                Ok((msg, payload)) => println!("channel '{}': {}", msg.get_channel_name(), payload),
+                Err(e) => println!("Failed to get message: {}", e),
+            }
+        }
+    });
+
+
+    let _handle2 = tokio::spawn(async move {
+        if let Err(e) = publish_message(redis_url_clone).await {
             println!("{:?}", e);
         };
     });
 
-    //
-    // receive message
-    //
-    loop {
-        let msg = pubsub.get_message()?;
-        let payload : String = msg.get_payload()?;
-        println!("channel '{}': {}", msg.get_channel_name(), payload);
-        
-       // break; // for test
-    };
+
+
+    // channelからreeive 1
+    let receiver1 = tokio::spawn(async move {
+        while let Ok(msg) = rx1.recv().await {
+            println!("受信機1: {}", msg);
+        }
+    });
+
+    // channelからreeive 2
+    let receiver2 = tokio::spawn(async move {
+        while let Ok(msg) = rx2.recv().await {
+            println!("受信機2: {}", msg);
+        }
+    });
+
+
+
+    // wait for the task to finish
+    let _ = tokio::join!(_handle1, _handle2);
 
     Ok(())
 }
